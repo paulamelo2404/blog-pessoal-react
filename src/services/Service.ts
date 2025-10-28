e0826ccbdf32bf5cdc1760609cc52bbb024ec904
Paula Melo <paulamelo2404@gmail.com>
2025-10-28 12:20:47 -0300

    logica de deletar tema criada

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -28,3 +28,7 @@ export const atualizar = async (url: string, dados: object, setDados: Function, 
     const resposta = await api.put(url, dados, header)
     setDados(resposta.data)
 }
+
+export const deletar = async (url: string, header: object) => {
+    await api.delete(url, header)
+}
