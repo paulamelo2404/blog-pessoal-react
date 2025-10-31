0f15697662d849c8fa0de29728fca2e5904c87f3
Paula Melo <paulamelo2404@gmail.com>
2025-10-31 12:05:08 -0300

    Navbar e Footer ocultos antes de loga usuario

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,12 @@
-import { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useContext, type ReactNode } from "react";
+import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
 
     const navigate = useNavigate();
 
-    const { handleLogout } = useContext(AuthContext)
+    const { usuario, handleLogout } = useContext(AuthContext)
 
     function logout() {
 
@@ -15,8 +15,12 @@ function Navbar() {
         navigate('/')
     }
 
-    return (
-        <>
+    let component: ReactNode
+
+    if (usuario.token !== "") {
+
+        component = (
+
             <div className='w-full flex justify-center py-4
             			   bg-indigo-900 text-white'>
             
@@ -24,14 +28,21 @@ function Navbar() {
                     <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
                     <div className='flex gap-4'>
-                       <Link to='/postagens' className='hover:underline'>Postagens</Link>
-                       <Link to='/temas' className='hover:underline'>Temas</Link>
-                       <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
+                        <Link to='/postagens' className='hover:underline'>Postagens</Link>
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
                         <Link to='/perfil' className='hover:underline'>Perfil</Link>
                         <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
                     </div>
                 </div>
             </div>
+
+        )
+    }
+
+    return (
+        <>
+            { component }
         </>
     )
 }
