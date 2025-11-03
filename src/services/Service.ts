1809bcf7a96c88b62743d1570c8891236965c86e
Paula Melo <paulamelo2404@gmail.com>
2025-11-03 18:15:19 -0300

    Configuração do Deploy

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 })
+
 export const cadastrarUsuario = async (url: string, dados: object, setDados: Function) => {
     const resposta = await api.post(url, dados)
     setDados(resposta.data)
